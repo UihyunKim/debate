dc2d09a385a84805da72a69078351f0f794d637c
KIM <stringee@gmail.com>
2018-04-25 16:01:37 +0200

    READ email UPDATE first and last name and display name OK

--- src/page-edit-profil.php
+++ src/page-edit-profil.php
@@ -7,15 +7,25 @@
 /* Load the registration file. */
 $error = array();
 
-// if( !is_user_logged_in() ) {
-// 	wp_redirect( home_url() );
-// 	exit;
-// }
+if( !is_user_logged_in() ) {
+	wp_redirect( home_url() );
+	exit;
+}
+
 
 /* If profile was saved, update profile. */
 if ( 'POST' == $_SERVER['REQUEST_METHOD'] && !empty( $_POST['action'] ) && $_POST['action'] == 'update-user' ) {
-    
     $current_user = wp_get_current_user();
+    // Check nonce
+    if( !isset( $_POST['_wpnonce'] ) || !wp_verify_nonce( $_POST['_wpnonce'], 'update-user' ) ) {
+        wp_redirect( get_permalink() . '?validation=unknown' );
+        exit;
+    }
+    // Check honeypot for automated bots
+    if( !empty($_POST['honey-name']) ) {
+		wp_redirect( get_permalink() . '?validation=unknown' );
+		exit;
+    }
 
     /* Update user password. */
     // if ( !empty($_POST['pass1'] ) && !empty( $_POST['pass2'] ) ) {
@@ -26,8 +36,8 @@ if ( 'POST' == $_SERVER['REQUEST_METHOD'] && !empty( $_POST['action'] ) && $_POS
     // }
 
     /* Update user information. */
-    if ( !empty( $_POST['url'] ) )
-        wp_update_user( array( 'ID' => $current_user->ID, 'user_url' => esc_url( $_POST['url'] ) ) );
+    // if ( !empty( $_POST['url'] ) )
+    //     wp_update_user( array( 'ID' => $current_user->ID, 'user_url' => esc_url( $_POST['url'] ) ) );
     
     // if ( !empty( $_POST['email'] ) ){
     //     if (!is_email(esc_attr( $_POST['email'] )))
@@ -39,10 +49,15 @@ if ( 'POST' == $_SERVER['REQUEST_METHOD'] && !empty( $_POST['action'] ) && $_POS
     //     }
     // }
 
-    if ( !empty( $_POST['first-name'] ) )
-        update_user_meta( $current_user->ID, 'first_name', esc_attr( $_POST['first-name'] ) );
     if ( !empty( $_POST['last-name'] ) )
+        $display_name = esc_attr( $_POST['last-name'] );
         update_user_meta($current_user->ID, 'last_name', esc_attr( $_POST['last-name'] ) );
+    if ( !empty( $_POST['first-name'] ) )
+    	$display_name .= esc_attr( $_POST['first-name'] );
+        update_user_meta( $current_user->ID, 'first_name', esc_attr( $_POST['first-name'] ) );
+    if ( $display_name ) {
+     	wp_update_user( array ('ID' => $current_user->ID, 'display_name' => esc_attr( $display_name ) ) );
+    }
     // if ( !empty( $_POST['description'] ) )
     //     update_user_meta( $current_user->ID, 'description', esc_attr( $_POST['description'] ) );
 
@@ -68,20 +83,22 @@ if ( 'POST' == $_SERVER['REQUEST_METHOD'] && !empty( $_POST['action'] ) && $_POS
                 <?php if ( count($error) > 0 ) {
                     echo '<p class="error">' . implode("<br />", $error) . '</p>'; 
                 } ?>
+                <?php echo '<h1>' . $current_user->display_name . '</h1>'; ?>
+                <?php printf( __( '<h1>%s</h1>', 'textdomain' ), get_the_author_meta( 'user_email', $current_user->ID ) ); ?>
                 <form method="post" id="adduser" action="<?php the_permalink(); ?>">
-                    <p class="form-username">
-                        <label for="first-name"><?php _e('First Name', 'profile'); ?></label>
-                        <input class="text-input" name="first-name" type="text" id="first-name" value="<?php the_author_meta( 'first_name', $current_user->ID ); ?>" />
-                    </p><!-- .form-username -->
                     <p class="form-username">
                         <label for="last-name"><?php _e('Last Name', 'profile'); ?></label>
                         <input class="text-input" name="last-name" type="text" id="last-name" value="<?php the_author_meta( 'last_name', $current_user->ID ); ?>" />
                     </p><!-- .form-username -->
+                    <p class="form-username">
+                        <label for="first-name"><?php _e('First Name', 'profile'); ?></label>
+                        <input class="text-input" name="first-name" type="text" id="first-name" value="<?php the_author_meta( 'first_name', $current_user->ID ); ?>" />
+                    </p><!-- .form-username -->
+                    <?php /*
                     <p class="form-email">
                         <label for="email"><?php _e('E-mail *', 'profile'); ?></label>
                         <input class="text-input" name="email" type="text" id="email" value="<?php the_author_meta( 'user_email', $current_user->ID ); ?>" />
                     </p><!-- .form-email -->
-                    <?php /*
                     <p class="form-url">
                         <label for="url"><?php _e('Website', 'profile'); ?></label>
                         <input class="text-input" name="url" type="text" id="url" value="<?php the_author_meta( 'user_url', $current_user->ID ); ?>" />
@@ -107,6 +124,7 @@ if ( 'POST' == $_SERVER['REQUEST_METHOD'] && !empty( $_POST['action'] ) && $_POS
                         <?php // echo $referer; ?>
                         <input name="updateuser" type="submit" id="updateuser" class="submit button" value="<?php _e('Update', 'profile'); ?>" />
                         <?php wp_nonce_field( 'update-user' ) ?>
+                        <input name="honey-name" value="" type="text" style="display:none;"></input>
                         <input name="action" type="hidden" id="action" value="update-user" />
                     </p><!-- .form-submit -->
                 </form><!-- #adduser -->

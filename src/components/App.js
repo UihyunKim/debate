9935450378af2dbdee9e8b6316dcc59185118be0
KIM <stringee@gmail.com>
2018-04-27 16:48:14 +0200

    testing traversy tutorial, added react part

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
+import Posts from "./Posts.js";
+import PostForm from "./Postform.js";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Hello there!</h1>
+        <PostForm />
+        <hr/>
+        <Posts />
       </div>
     )
   }
